54c97b0c330c8906cda3b72cdb000538cab368dc
Winkler30 <charleswinkler76@gmail.com>
2023-09-08 06:45:00 -0300

    feat: array

---
+++ aula-07.js
@@ -0,0 +1,14 @@
+const idadeCharles = 18;
+const idadeSouza = 18;
+const idadeSena = 18;
+const idadeJulio = 18;
+
+
+const idades = [10,20,30];
+
+for(let i = 0; i < idades.length; i++){
+    console.log('Array',idades[i]);
+}
+
+console.log('Resultado', idades[2]);
+console.log('Resultado', idades.length);
