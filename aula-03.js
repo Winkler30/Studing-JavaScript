d428a63cfbd57c403bc18d8f6e7f54da5a6650b0
Charles Winkler <charleswinkler76@gmail.com>
2023-09-08 06:13:10 -0300

    feat: if and else

---
+++ aula-03.js
@@ -0,0 +1,8 @@
+const idade = 8;
+const nome = 'Charles';
+
+if (idade >= 18 && nome === 'Charles') {//if (idade >= 18 ||   nome === 'Charles')
+    console.log(`Pode entrar!`);
+} else {
+    console.log(`Não pode entrar!`);
+}
