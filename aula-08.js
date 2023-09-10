ec09f340c5974494f7e41e05234e1a6b8af9a7d5
Winkler30 <charleswinkler76@gmail.com>
2023-09-09 21:35:40 -0300

    feat: convert

---
+++ aula-08.js
@@ -0,0 +1,10 @@
+const salario = 104
+const salarioString = '110'
+const salarioConvertido = Number(salarioString)
+
+console.log(salario.toFixed(2))
+console.log(Number(salarioString).toFixed(2))
+console.log(salarioConvertido)
+
+//Tomar cuidado com o ponto flutuante do JS, 
+//pois pode chegar ao infinito
