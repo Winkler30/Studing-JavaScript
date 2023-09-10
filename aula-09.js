15215b766402b71a64ebc61b5e4a81179a991574
Winkler30 <charleswinkler76@gmail.com>
2023-09-09 21:45:16 -0300

    feat: function

---
+++ aula-09.js
@@ -0,0 +1,11 @@
+function soma (item1,item2) {
+    return item1 + item2;
+}
+function diminuir (item1,item2) {
+    return item1 - item2;
+}
+
+console.log('Resultado da função soma:',soma(12,20));
+console.log('Resultado da função diminuir:',diminuir(12,20));
+// Na função podemos criar logica, e retornar o resultado
+// dentro do console.log, podemos chamar a função diretamente
