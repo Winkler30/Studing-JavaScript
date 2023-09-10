3063d8022ecd38261a1bee0777df2f3323222b53
Winkler30 <charleswinkler76@gmail.com>
2023-09-10 14:10:03 -0300

    switch case

---
+++ aula-17.js
@@ -0,0 +1,40 @@
+
+function teste(key){
+    switch (key) {
+        case 'Charles':
+            console.log('Charles aqui')
+            break;
+        case 'Julio':
+            console.log('Julio aqui')
+            break;
+        case 'Souza':
+            console.log('Souza aqui')
+            break;
+        default:
+            console.log('Nenhum dos nomes')
+            break;
+    }
+
+}
+teste('Charles');
+teste('Julio');
+teste('Souza');
+
+/*
+function teste(key){
+    switch (key) {
+        case 'Charles':
+            return'Charles aqui'
+        case 'Julio':
+            return'Julio aqui'
+        case 'Souza':
+            return'Souza aqui'
+        default:
+            return'Nenhum dos nomes'
+    }
+}
+
+console.log(teste('Charles'));
+ */
+
+
