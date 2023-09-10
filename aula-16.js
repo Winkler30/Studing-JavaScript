59bd35785f9d0aa438365881966420496dd82cb8
Winkler30 <charleswinkler76@gmail.com>
2023-09-10 13:53:20 -0300

    array push

---
+++ aula-16.js
@@ -0,0 +1,13 @@
+const numero = 43;
+const stringA = "43";
+const verdadeOuFalso = true;// ou false
+
+const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arrayTest.push(10);
+arrayTest.unshift(0);
+
+arrayTest.shift();// REMOVE O PRIMEIRO ELEMENTO DO ARRAY
+arrayTest.pop(); // REMOVE O ULTIMO ELEMENTO DO ARRAY
+
+console.log(arrayTest);
